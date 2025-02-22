18282260267fabb6903c84e966a9772e511987f8
Peter <peter@whiteknightinc.com>
2025-02-22 17:45:38 -0500

    input component and login

--- components/Input.tsx
+++ components/Input.tsx
@@ -1,13 +1,20 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, TextInput, View } from "react-native";
 import React from "react";
 import { InputProps } from "@/types";
+import { colors, radius, spacingX } from "@/constants/theme";
+import { verticalScale } from "@/utils/styling";
 
 const Input = (props: InputProps) => {
   return (
     <View
       style={[styles.container, props.containerStyle && props.containerStyle]}
     >
-      <Text>Input</Text>
+      {props.icon && props.icon}
+      <TextInput
+        style={[styles.input, props.inputStyle]}
+        placeholderTextColor={colors.neutral400}
+        ref={props.inputRef && props.inputRef}
+      />
     </View>
   );
 };
@@ -15,6 +22,22 @@ const Input = (props: InputProps) => {
 export default Input;
 
 const styles = StyleSheet.create({
-  container: {},
+  container: {
+    flexDirection: "row",
+    height: verticalScale(54),
+    alignItems: "center",
+    justifyContent: "center",
+    borderWidth: 1,
+    borderColor: colors.neutral300,
+    borderRadius: radius._17,
+    borderCurve: "continuous",
+    paddingHorizontal: spacingX._15,
+    gap: spacingX._10,
+  },
+  input: {
+    flex: 1,
+    color: colors.white,
+    fontSize: verticalScale(14),
+  },
 });
-//8.21
+//11.36
