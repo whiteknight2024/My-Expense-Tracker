4afafdba73d3f90dc5328605968c93d052783763
Peter <peter@whiteknightinc.com>
2025-02-23 16:02:30 -0500

    added firebase via npm and created the config firebase ts file with info from firebase itself during signup

---
+++ config/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCoS0NfCPrdVKXzfPI34MnVGk023VqYn4M",
+  authDomain: "expense-tracker-91906.firebaseapp.com",
+  projectId: "expense-tracker-91906",
+  storageBucket: "expense-tracker-91906.firebasestorage.app",
+  messagingSenderId: "446789932622",
+  appId: "1:446789932622:web:ba202dc1662c3281dc551f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
