4b03ca99ba5ae98ff20762a9e8b444bbd309cd68
Peter <peter@whiteknightinc.com>
2025-02-24 15:31:24 -0500

    header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,10 +1,12 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
+import Typo from "./Typo";
+import { HeaderProps } from "@/types";
 
-const Header = () => {
+const Header = ({ title = "", leftIcon, style }: HeaderProps) => {
   return (
     <View>
-      <Text>Header</Text>
+      <Typo>Header</Typo>
     </View>
   );
 };
@@ -12,3 +14,4 @@ const Header = () => {
 export default Header;
 
 const styles = StyleSheet.create({});
+//video 6 1:10s
