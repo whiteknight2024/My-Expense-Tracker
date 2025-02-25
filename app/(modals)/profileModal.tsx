5fcca66a6e7abca428975090aa4d98b1246c60d0
Peter <peter@whiteknightinc.com>
2025-02-25 09:50:49 -0500

    update profile modal with styling and screenwrapper

--- app/(modals)/profileModal.tsx
+++ app/(modals)/profileModal.tsx
@@ -1,15 +1,69 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
+import { scale, verticalScale } from "@/utils/styling";
+import { colors, spacingX, spacingY } from "@/constants/theme";
+import ScreenWrapper from "@/components/ScreenWrapper";
 
 const ProfileModal = () => {
   return (
-    <View>
+    <ScreenWrapper>
       <Text>ProfileModal</Text>
-    </View>
+    </ScreenWrapper>
   );
 };
 
 export default ProfileModal;
 
-const styles = StyleSheet.create({});
-//22.02
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "space-between",
+    paddingHorizontal: spacingY._20,
+  },
+  footer: {
+    alignItems: "center",
+    flexDirection: "row",
+    justifyContent: "center",
+    paddingHorizontal: spacingX._20,
+    gap: scale(12),
+    paddingTop: spacingY._15,
+    borderTopColor: colors.neutral700,
+    marginBottom: spacingY._5,
+    borderTopWidth: 1,
+  },
+  form: {
+    gap: spacingY._30,
+    marginTop: spacingY._15,
+  },
+  avatarContainer: {
+    position: "relative",
+    alignSelf: "center",
+  },
+  avatar: {
+    alignSelf: "center",
+    backgroundColor: colors.neutral300,
+    height: verticalScale(135),
+    width: verticalScale(135),
+    borderRadius: 200,
+    borderWidth: 1,
+    borderColor: colors.neutral500,
+    // overflow: "hidden",
+    // position: "relative",
+  },
+  editIcon: {
+    position: "absolute",
+    bottom: spacingY._5,
+    right: spacingY._7,
+    borderRadius: 100,
+    backgroundColor: colors.neutral100,
+    shadowColor: colors.black,
+    shadowOffset: { width: 0, height: 0 },
+    shadowOpacity: 0.25,
+    shadowRadius: 10,
+    elevation: 4,
+    padding: spacingY._7,
+  },
+  inputContainer: {
+    gap: spacingY._10,
+  },
+});
