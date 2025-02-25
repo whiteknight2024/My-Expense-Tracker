46c3c4c768a654687e0816c9974b93c9efa2cccb
Peter <peter@whiteknightinc.com>
2025-02-25 09:52:05 -0500

    create a modealwrapper

---
+++ components/ModalWrapper.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const ModalWrapper = () => {
+  return (
+    <View>
+      <Text>ModalWrapper</Text>
+    </View>
+  );
+};
+
+export default ModalWrapper;
+
+const styles = StyleSheet.create({});
