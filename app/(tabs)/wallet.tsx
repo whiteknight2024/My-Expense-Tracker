9727bd7b17f79973ab724a9d712654bad8924c95
Peter <peter@whiteknightinc.com>
2025-02-25 22:08:11 -0500

    updated wallet stylesheet

--- app/(tabs)/wallet.tsx
+++ app/(tabs)/wallet.tsx
@@ -2,10 +2,12 @@ import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 import ScreenWrapper from "@/components/ScreenWrapper";
 import Typo from "@/components/Typo";
+import { colors, radius, spacingX, spacingY } from "@/constants/theme";
+import { verticalScale } from "@/utils/styling";
 
 const Wallet = () => {
   return (
-    <ScreenWrapper>
+    <ScreenWrapper style={{ backgroundColor: colors.black }}>
       <Typo>Wallet</Typo>
     </ScreenWrapper>
   );
@@ -13,4 +15,33 @@ const Wallet = () => {
 
 export default Wallet;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "space-between",
+  },
+  balanceView: {
+    height: verticalScale(160),
+    backgroundColor: colors.black,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  flexRow: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: spacingY._10,
+  },
+  wallets: {
+    flex: 1,
+    backgroundColor: colors.neutral900,
+    borderTopRightRadius: radius._30,
+    borderTopLeftRadius: radius._30,
+    padding: spacingX._20,
+    paddingTop: spacingX._25,
+  },
+  listStyle: {
+    paddingVertical: spacingY._25,
+    paddingTop: spacingY._15,
+  },
+});
--- constants/index.ts
+++ constants/index.ts
@@ -1,3 +1,2 @@
 export const CLOUDINARY_CLOUD_NAME = "dnwjr1eak";
 export const CLOUDINARY_UPLOAD_PRESET = "images";
-//https://api.cloudinary.com/v1_1/dnwjr1eak/image/upload
