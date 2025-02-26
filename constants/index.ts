2a5ab03a9931caf3dd17d505f94c7bfb36b4b31f
Peter <peter@whiteknightinc.com>
2025-02-25 20:27:05 -0500

    sign up cloudinary, constants file values, imageService uploadFileToCloudinary function, install axios

--- constants/index.ts
+++ constants/index.ts
@@ -1,2 +1,3 @@
-export const CLOUDINARY_CLOUD_NAME = "";
-export const CLOUDINARY_UPLOAD_PRESET = "";
+export const CLOUDINARY_CLOUD_NAME = "dnwjr1eak";
+export const CLOUDINARY_UPLOAD_PRESET = "images";
+//https://api.cloudinary.com/v1_1/dnwjr1eak/image/upload
--- services/imageService.ts
+++ services/imageService.ts
@@ -1,3 +1,14 @@
+import { ResponseType } from "@/types";
+
+const CLOUDINARY_CLOUD_URL = "https://api.cloudinary.com/v1_1/${CLOUDINARY_CLOUD_NAME}/image/upload";
+
+
+export const uploadFileToCloudinary = async (
+    file: {uri?: string} | string,
+    folderName: string
+    
+): Promise<ResponseType>
+
 export const getProfileImage = (file: any)=>{
     if(file && typeof file == 'string') return file;
     if(file && typeof file == 'object') return file.uri;
