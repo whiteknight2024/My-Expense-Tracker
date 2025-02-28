9fe88567cb846a34956ba85eae218b6ef81e38d7
Peter <peter@whiteknightinc.com>
2025-02-28 11:48:06 -0500

    homecard add styling and view etc for displaying data

--- components/HomeCard.tsx
+++ components/HomeCard.tsx
@@ -1,14 +1,71 @@
-import { StyleSheet, Text, View } from "react-native";
+import { ImageBackground, StyleSheet, Text, View } from "react-native";
 import React from "react";
+import Typo from "./Typo";
+import { colors, spacingX, spacingY } from "@/constants/theme";
+import { scale, verticalScale } from "@/utils/styling";
+import * as Icons from "phosphor-react-native";
 
 const HomeCard = () => {
   return (
-    <View>
-      <Text>HomeCard</Text>
-    </View>
+    <ImageBackground
+      source={require("../assets/images/card.png")}
+      resizeMode="stretch"
+      style={styles.bgImage}
+    >
+      <View style={styles.container}>
+        <View>
+          {/* total balance */}
+          <View style={styles.totalBalanceRow}>
+            <Typo color={colors.neutral800} size={17} fontWeight={"500"}>
+              Total Balance
+            </Typo>
+            <Icons.DotsThreeOutline
+              size={verticalScale(23)}
+              color={colors.black}
+              weight="fill"
+            />
+          </View>
+          <Typo color={colors.black} size={30} fontWeight={"bold"}>
+            $2300.00
+          </Typo>
+        </View>
+      </View>
+    </ImageBackground>
   );
 };
 
 export default HomeCard;
-
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  bgImage: {
+    height: scale(210),
+    width: "100%",
+  },
+  container: {
+    padding: spacingX._20,
+    paddingHorizontal: scale(23),
+    height: "87%",
+    width: "100%",
+    justifyContent: "space-between",
+  },
+  totalBalanceRow: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: spacingY._5,
+  },
+  stats: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  statsIcon: {
+    backgroundColor: colors.neutral350,
+    padding: spacingY._5,
+    borderRadius: 50,
+  },
+  incomeExpense: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: spacingY._7,
+  },
+});
