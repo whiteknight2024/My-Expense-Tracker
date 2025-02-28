fcea0e61b288c989eb7e773ae6de24229ab016c2
Peter <peter@whiteknightinc.com>
2025-02-27 22:05:29 -0500

    update home page

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,27 +2,60 @@ import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 // import Button from "@/components/Button";
 import Typo from "@/components/Typo";
-// import { colors } from "@/constants/theme";
-// import { signOut } from "firebase/auth";
-// import { auth } from "@/config/firebase";
+import { auth } from "@/config/firebase";
 import ScreenWrapper from "@/components/ScreenWrapper";
+import { colors, spacingX, spacingY } from "@/constants/theme";
+import { verticalScale } from "@/utils/styling";
+import { useAuth } from "@/contexts/authContext";
 
 //22.58 add logout button
 const Home = () => {
-  // const handleLogout = async () => {
-  //   await signOut(auth);
-  // };
+  const { user } = useAuth();
 
   return (
     <ScreenWrapper>
-      <Typo>Home</Typo>
-      {/* <Button onPress={handleLogout}>
-        <Typo color={colors.black}>Logout</Typo>
-      </Button> */}
+      <View style={styles.container}>
+        {/* header */}
+        <View style={styles.header}>
+          <View style={{ gap: 4 }}>
+            <Typo>Hello,</Typo>
+          </View>
+        </View>
+      </View>
     </ScreenWrapper>
   );
 };
 
 export default Home;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingHorizontal: spacingX._20,
+    marginTop: verticalScale(8),
+  },
+  header: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: spacingY._10,
+  },
+  searchIcon: {
+    backgroundColor: colors.neutral700,
+    padding: spacingX._10,
+    borderRadius: 50,
+  },
+  floatingButton: {
+    height: verticalScale(50),
+    width: verticalScale(50),
+    borderRadius: 100,
+    position: "absolute",
+    bottom: verticalScale(30),
+    right: verticalScale(30),
+  },
+  scrollViewStyle: {
+    marginTop: spacingY._10,
+    paddingBottom: verticalScale(100),
+    gap: spacingY._25,
+  },
+});
