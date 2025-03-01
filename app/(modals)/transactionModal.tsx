e139bae392f713fa2dd06b0ca2475ddc36e396d6
Peter <peter@whiteknightinc.com>
2025-02-28 22:06:46 -0500

    added dropdown to transaction modal

--- app/(modals)/transactionModal.tsx
+++ app/(modals)/transactionModal.tsx
@@ -112,6 +112,30 @@ const TransactionModal = () => {
     );
   };
 
+  const data = [
+    { label: "Item 1", value: "1" },
+    { label: "Item 2", value: "2" },
+    { label: "Item 3", value: "3" },
+    { label: "Item 4", value: "4" },
+    { label: "Item 5", value: "5" },
+    { label: "Item 6", value: "6" },
+    { label: "Item 7", value: "7" },
+    { label: "Item 8", value: "8" },
+  ];
+  const [value, setValue] = useState(null);
+  const [isFocus, setIsFocus] = useState(false);
+
+  const renderLabel = () => {
+    if (value || isFocus) {
+      return (
+        <Text style={[styles.label, isFocus && { color: "blue" }]}>
+          Dropdown label
+        </Text>
+      );
+    }
+    return null;
+  };
+
   return (
     <ModalWrapper>
       <View style={styles.container}>
@@ -128,6 +152,35 @@ const TransactionModal = () => {
           <View style={styles.inputContainer}>
             <Typo color={colors.neutral200}>Type</Typo>
             {/* dropdown */}
+            <Dropdown
+              style={[styles.dropdown, isFocus && { borderColor: "blue" }]}
+              placeholderStyle={styles.placeholderStyle}
+              selectedTextStyle={styles.selectedTextStyle}
+              inputSearchStyle={styles.inputSearchStyle}
+              iconStyle={styles.iconStyle}
+              data={data}
+              search
+              maxHeight={300}
+              labelField="label"
+              valueField="value"
+              placeholder={!isFocus ? "Select item" : "..."}
+              searchPlaceholder="Search..."
+              value={value}
+              onFocus={() => setIsFocus(true)}
+              onBlur={() => setIsFocus(false)}
+              onChange={(item) => {
+                setValue(item.value);
+                setIsFocus(false);
+              }}
+              //   renderLeftIcon={() => (
+              //     <AntDesign
+              //       style={styles.icon}
+              //       color={isFocus ? "blue" : "black"}
+              //       name="Safety"
+              //       size={20}
+              //     />
+              //   )}
+            />
           </View>
           <View style={styles.inputContainer}>
             <Typo color={colors.neutral200}>Transaction Icon</Typo>
