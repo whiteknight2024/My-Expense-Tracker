049fa3197c1c4e49d21530471f37f195bcd37277
Peter <peter@whiteknightinc.com>
2025-03-08 22:03:49 -0500

    statistics page styling

--- app/(tabs)/statistics.tsx
+++ app/(tabs)/statistics.tsx
@@ -2,6 +2,8 @@ import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 import ScreenWrapper from "@/components/ScreenWrapper";
 import Typo from "@/components/Typo";
+import { colors, radius, spacingX, spacingY } from "@/constants/theme";
+import { scale, verticalScale } from "@/utils/styling";
 
 const Statistics = () => {
   return (
@@ -13,4 +15,44 @@ const Statistics = () => {
 
 export default Statistics;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  chartContainer: {
+    position: "relative",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  chartLoadingContainer: {
+    position: "absolute",
+    width: "100%",
+    height: "100%",
+    borderRadius: radius._12,
+    backgroundColor: "rgba(0,0,0,0.6)",
+  },
+  header: {},
+  noChart: {
+    backgroundColor: "rgba(0,0,0,0.6)",
+    height: verticalScale(210),
+  },
+  searchIcon: {
+    backgroundColor: colors.neutral700,
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 100,
+    height: verticalScale(35),
+    width: verticalScale(35),
+    borderCurve: "continuous",
+  },
+  segmentStyle: {
+    height: scale(37),
+  },
+  segmentFontStyle: {
+    fontSize: verticalScale(13),
+    fontWeight: "bold",
+    color: colors.black,
+  },
+  container: {
+    paddingHorizontal: spacingX._20,
+    paddingVertical: spacingY._5,
+    gap: spacingY._10,
+  },
+});
