3c324d1c421909f2d2c507a995afd97d07fb944f
Peter <peter@whiteknightinc.com>
2025-03-10 23:14:22 -0400

    update scale for label width for monthly and yearly

--- services/transactionService.ts
+++ services/transactionService.ts
@@ -402,7 +402,7 @@ export const fetchMonthlyStats = async (uid: string): Promise<ResponseType> => {
         value: month.income,
         label: month.month,
         spacing: scale(4),
-        labelWidth: scale(46),
+        labelWidth: scale(50),
         frontColor: colors.primary, // Income bar color
       },
       {
@@ -480,7 +480,7 @@ export const fetchYearlyStats = async (uid: string): Promise<ResponseType> => {
         value: year.income,
         label: year.year,
         spacing: scale(4),
-        labelWidth: scale(35),
+        labelWidth: scale(50),
         frontColor: colors.primary, // Income bar color
       },
       {
