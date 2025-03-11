902f5d3d3cf766292dc1a5ca72a671235fb15998
Peter <peter@whiteknightinc.com>
2025-03-11 11:20:37 -0400

    update search modal

--- app/(modals)/searchModal.tsx
+++ app/(modals)/searchModal.tsx
@@ -1,16 +1,114 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
-import ScreenWrapper from "@/components/ScreenWrapper";
+import {
+  Alert,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import React, { useEffect, useState } from "react";
+import { scale, verticalScale } from "@/utils/styling";
+import { colors, spacingX, spacingY } from "@/constants/theme";
+import ModalWrapper from "@/components/ModalWrapper";
+import Header from "@/components/Header";
+import BackButton from "@/components/BackButton";
+import { Image } from "expo-image";
+import { getProfileImage } from "@/services/imageService";
+import { useAuth } from "@/contexts/authContext";
+import * as Icons from "phosphor-react-native";
 import Typo from "@/components/Typo";
+import Input from "@/components/Input";
+import { UserDataType, WalletType } from "@/types";
+import Button from "@/components/Button";
+import { updateUser } from "@/services/userService";
+import { useLocalSearchParams, useRouter } from "expo-router";
+import * as ImagePicker from "expo-image-picker";
+import ImageUpload from "@/components/ImageUpload";
+import { createOrUpdateWallet, deleteWallet } from "@/services/walletService";
 
+//7.42 video 8
+//18.21 vid 9s
 const SearchModal = () => {
+  const { user, updateUserData } = useAuth();
+  const [loading, setLoading] = useState(false);
+  const router = useRouter();
+  const [search, setSearch] = useState("");
+
   return (
-    <ScreenWrapper>
-      <Typo>SearchModal</Typo>
-    </ScreenWrapper>
+    <ModalWrapper>
+      <View style={styles.container}>
+        <Header
+          title={"Search"}
+          leftIcon={<BackButton />}
+          style={{ marginBottom: spacingY._10 }}
+        />
+        {/* Form */}
+        <ScrollView contentContainerStyle={styles.form}>
+          <View style={styles.inputContainer}>
+            <Input
+              placeholder="shoes..."
+              value={search}
+              onChangeText={(value) => setSearch(value)}
+            />
+          </View>
+        </ScrollView>
+      </View>
+    </ModalWrapper>
   );
 };
 
 export default SearchModal;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "space-between",
+    paddingHorizontal: spacingY._20,
+  },
+  footer: {
+    alignItems: "center",
+    flexDirection: "row",
+    justifyContent: "center",
+    paddingHorizontal: spacingX._20,
+    gap: scale(12),
+    paddingTop: spacingY._15,
+    borderTopColor: colors.neutral700,
+    marginBottom: spacingY._5,
+    borderTopWidth: 1,
+  },
+  form: {
+    gap: spacingY._30,
+    marginTop: spacingY._15,
+  },
+  avatarContainer: {
+    position: "relative",
+    alignSelf: "center",
+  },
+  avatar: {
+    alignSelf: "center",
+    backgroundColor: colors.neutral300,
+    height: verticalScale(135),
+    width: verticalScale(135),
+    borderRadius: 200,
+    borderWidth: 1,
+    borderColor: colors.neutral500,
+    // overflow: "hidden",
+    // position: "relative",
+  },
+  editIcon: {
+    position: "absolute",
+    bottom: spacingY._5,
+    right: spacingY._7,
+    borderRadius: 100,
+    backgroundColor: colors.neutral100,
+    shadowColor: colors.black,
+    shadowOffset: { width: 0, height: 0 },
+    shadowOpacity: 0.25,
+    shadowRadius: 10,
+    elevation: 4,
+    padding: spacingY._7,
+  },
+  inputContainer: {
+    gap: spacingY._10,
+  },
+});
