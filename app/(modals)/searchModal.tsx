12e81c7f1ea269816c442abff10e4e9e08591b26
Peter <peter@whiteknightinc.com>
2025-03-11 11:39:34 -0400

    update search modal

--- app/(modals)/searchModal.tsx
+++ app/(modals)/searchModal.tsx
@@ -26,8 +26,7 @@ import * as ImagePicker from "expo-image-picker";
 import ImageUpload from "@/components/ImageUpload";
 import { createOrUpdateWallet, deleteWallet } from "@/services/walletService";
 
-//7.42 video 8
-//18.21 vid 9s
+//2.51 vid 15
 const SearchModal = () => {
   const { user, updateUserData } = useAuth();
   const [loading, setLoading] = useState(false);
@@ -35,7 +34,7 @@ const SearchModal = () => {
   const [search, setSearch] = useState("");
 
   return (
-    <ModalWrapper>
+    <ModalWrapper style={{ backgroundColor: colors.neutral900 }}>
       <View style={styles.container}>
         <Header
           title={"Search"}
@@ -49,6 +48,7 @@ const SearchModal = () => {
               placeholder="shoes..."
               value={search}
               onChangeText={(value) => setSearch(value)}
+              containerStyle={{ backgroundColor: colors.neutral800 }}
             />
           </View>
         </ScrollView>
@@ -65,17 +65,6 @@ const styles = StyleSheet.create({
     justifyContent: "space-between",
     paddingHorizontal: spacingY._20,
   },
-  footer: {
-    alignItems: "center",
-    flexDirection: "row",
-    justifyContent: "center",
-    paddingHorizontal: spacingX._20,
-    gap: scale(12),
-    paddingTop: spacingY._15,
-    borderTopColor: colors.neutral700,
-    marginBottom: spacingY._5,
-    borderTopWidth: 1,
-  },
   form: {
     gap: spacingY._30,
     marginTop: spacingY._15,
@@ -84,30 +73,7 @@ const styles = StyleSheet.create({
     position: "relative",
     alignSelf: "center",
   },
-  avatar: {
-    alignSelf: "center",
-    backgroundColor: colors.neutral300,
-    height: verticalScale(135),
-    width: verticalScale(135),
-    borderRadius: 200,
-    borderWidth: 1,
-    borderColor: colors.neutral500,
-    // overflow: "hidden",
-    // position: "relative",
-  },
-  editIcon: {
-    position: "absolute",
-    bottom: spacingY._5,
-    right: spacingY._7,
-    borderRadius: 100,
-    backgroundColor: colors.neutral100,
-    shadowColor: colors.black,
-    shadowOffset: { width: 0, height: 0 },
-    shadowOpacity: 0.25,
-    shadowRadius: 10,
-    elevation: 4,
-    padding: spacingY._7,
-  },
+
   inputContainer: {
     gap: spacingY._10,
   },
